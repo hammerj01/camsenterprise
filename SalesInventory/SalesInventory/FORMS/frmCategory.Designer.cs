9b0b5f9304ff6fd3dcba0ece77d0aa6f03ca72ee
hammerj01 <hmertojr01@gmail.com>
2015-09-10 23:47:35 -0700

    new
    new ones

--- SalesInventory/SalesInventory/FORMS/frmCategory.Designer.cs
+++ SalesInventory/SalesInventory/FORMS/frmCategory.Designer.cs
@@ -69,7 +69,7 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(419, 322);
+            this.button1.Location = new System.Drawing.Point(278, 299);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 53);
             this.button1.TabIndex = 3;
@@ -99,7 +99,7 @@
             // 
             this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSave.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(563, 322);
+            this.btnSave.Location = new System.Drawing.Point(422, 299);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(136, 53);
             this.btnSave.TabIndex = 6;
